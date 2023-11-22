579459e62993d5142b1043405beb52f25a63bac5
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-11-23 03:06:06 +0530

    Update postcard.jsx

--- components/postcard.jsx
+++ components/postcard.jsx
@@ -1,13 +1,13 @@
-// import React from "react";
+import React from "react";
 
-// const PostCard = (props) => {
-//     return(
-//         <div className="post-card">
-//             <h3>{props.title}</h3>
-//             <p>{props.body}</p>
-//         </div>
-//     );
-// };
+const PostCard = (props) => {
+    return(
+        <div className="post-card">
+            <h3>{props.title}</h3>
+            <p>{props.body}</p>
+        </div>
+    );
+};
 
 
-// export default PostCard;
+export default PostCard;
