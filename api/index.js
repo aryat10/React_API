de49229f697c06f16b72b700c805151b06c44a2d
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-11-23 03:19:43 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ export const getPosts = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
         method: 'GET',
     });
-    return response.json();                                                                // Above code is use call an API 
+    return response.json();                                                                
 };
 
 export const getRandomUser = async() =>{
     const response = await fetch('https://randomuser.me/api/', {
-        method: 'GET'                                                                       // Above code is use call an API 
+        method: 'GET'                                                                       
     });
     return await response.json();
 };
