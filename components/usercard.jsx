159c307167a9d42764621a15d1a23e1b87724235
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-11-23 03:22:25 +0530

    Update usercard.jsx

--- components/usercard.jsx
+++ components/usercard.jsx
@@ -4,7 +4,6 @@ const UserCard = (props) => {
     console.log(props.data)
     return(
         <div className="user-card">
-            {/* <img className="user-img"/> */}
             <h3>{props.data.name.first}</h3>
             <p>{props.data.phone}</p>
             <p>{props.data.location.city}</p>
